9ada58bee86f9b1713f5c101ed5e08afac41a5ff
yevgeniy.tkachenko <tkach.ossystem@gmail.com>
2021-05-28 17:15:12 +0300

    28.05.2021

--- src/js/components/Header/index.js
+++ src/js/components/Header/index.js
@@ -19,6 +19,8 @@ export default class Header extends Component {
         super(props);
         this.state = {
             name: '',
+            phone: '',
+            email: '',
             clicked: false,
             error: false,
             distance: "",
@@ -67,8 +69,103 @@ export default class Header extends Component {
         let name = e.target.value;
         this.setState({ name: name});
     }
+    onPhoneChange = (e) => {
+        let phone = e.target.value;
+        this.setState({ phone: phone});
+    }
+    onEmailChange = (e) => {
+        let email = e.target.value;
+        this.setState({ email: email});
+    }
     
-    onAsync = (e) => {
+    onGetData = async (e) => {
+        e.preventDefault();
+        let phone = this.state.phone
+        let email = this.state.email
+        console.log(phone)
+        console.log(email)
+
+
+        // let res = phone.match(/var/gi)
+        // console.log(res)
+        let str = `7 88 77 6655 23_a./34 1_wvTTRRasia`
+        let str2 = `+38(098) 63-45-167black Вася и Петя`
+        let str4 = `1) это метеочувствительные люди,
+                    2) имеющие хронические заболевания
+                       a) пожилые
+                       b)сердечники,
+                    3) гипертоники и гипотоники,
+                    4) люди с бессонницей.`;
+        let title = `vasia343434`
+        let nameMask = /^[a-zA-ZäöüÄÖÜß\u0400-\u04FF-\s]*$/i
+        let emailTest = `vasia@gmail.com`
+        let time = '12:37:34'
+        let timeMask = /^\d\d:\d\d:\d\d$/
+        let phoneMask = /^[\d]+$/ig
+        let emailMask = /@/i
+        let citeMask = /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/
+        let number = "+7(903)-123-45-67";
+        let errors = {}
+        const testInput = (key, value, rule) => {
+            if(!value.match(rule)){
+                errors[key] = 'invalid'
+                return true
+            }
+            if(key in errors)  delete errors[key]
+            return false
+        }
+        testInput('phone', phone, phoneMask)
+        testInput('email', email, emailMask)
+        console.log(errors)
+
+      
+        
+        //let res2 = str.replace(/^[\d-./]+/i, '0987656166')
+        //console.log(res2)
+       // console.log( str.match(/\s/ig).join('') )
+        //console.log( str2.replace(/CSS.4/ig, '') )
+        // console.log( str2.match(/^.[\d\s()-]+/i).join('') )
+        // console.log( str2.match(/[\d]+/ig).join('') )
+        //console.log( str2.match(/[a-z]/ig).join('') )
+        //console.log( str2.match(/^.+[а-я]+$/))
+        //console.log( str4.match(/^[1-2a-z]/gm).join('') )
+        // console.log( titleMask.test(title) )
+        //console.log( emailTest.match(emailMask) )
+        // console.log( /^[a-zA-Zа-я]+$/i.test(str3) )
+        //regexp.test(str)
+        
+        // if(!res){
+        //     console.log('error')
+        // }
+        
+
+        // async function getData() {
+        //     //return Promise.reject('errro')
+        //     return phone
+        //     // let user
+        //     // let response = await fetch('https://jsonplaceholder.typicode.com/users');
+        //     // if (response.ok) { // если HTTP-статус в диапазоне 200-299
+        //     //     user = await response.json(); // получаем тело ответа (см. про этот метод ниже)
+        //     // } else {
+        //     //     alert("Ошибка HTTP: " + response.status);
+        //     // }
+        //     // console.log(user)
+        //     // let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+        //     // let res = await fetch(url);
+        //     // let commits = await res.json(); // читаем ответ в формате JSON
+        //     // console.log(commits)
+        //     //let promise = Promise.resolve(inputName)
+        //
+        //     // let promise2 = new Promise((resolve, reject) => {
+        //     //     setTimeout(() => reject('error '+inputName), 1700)
+        //     // });
+        //     // let error = await promise2;
+        // }
+        // let data = await getData()
+        // return data
+    }
+    
+    onGetStatus = (e) => {
         e.preventDefault();
         let inputName =  this.state.name;
         const statusArr = {
@@ -80,8 +177,7 @@ export default class Header extends Component {
             401:' Unauthorized',
             empty:' Status not found'
         }
-        
-        async function testAsync() {
+        async function findStatus(){
             //return Promise.reject(401)
             let promise = new Promise((resolve, reject) => {
                 if(inputName < 400){
@@ -89,20 +185,6 @@ export default class Header extends Component {
                 }
                 setTimeout(() => reject(inputName), 1700)
             });
-            let user
-            let response = await fetch('https://jsonplaceholder.typicode.com/users');
-            if (response.ok) { // если HTTP-статус в диапазоне 200-299
-                user = await response.json(); // получаем тело ответа (см. про этот метод ниже)
-            } else {
-                alert("Ошибка HTTP: " + response.status);
-            }
-            console.log(user)
-            let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-            let res = await fetch(url);
-            let commits = await res.json(); // читаем ответ в формате JSON
-            console.log(commits)
-            //let promise = Promise.resolve(inputName)
-
             
             let status = ''
             try {
@@ -112,32 +194,22 @@ export default class Header extends Component {
                 alert(err); // TypeError: failed to fetch
                 return Promise.resolve(err);
             }
-            // let promise2 = new Promise((resolve, reject) => {
-            //     setTimeout(() => reject('error '+inputName), 1700)
-            // });
-           // let error = await promise2;
-           
-            //return Promise.reject(inputName)
-           
-           //console.log(status.toString()+statusArr[status])
-            //console.log(error)
-            
             return status
         }
-        testAsync()
+        findStatus()
           .finally(console.log('loading response'))
           .then(
-              (status) => {
-                  if(statusArr[status]){
-                      console.log(status.toString()+statusArr[status])
-                  }else{
-                      console.log(status.toString()+statusArr['empty'])
-                  }
-              },
-              (error) => {
-                  console.log('error '+error.toString()+statusArr[error])
-              }
-        )
+            (status) => {
+                if(statusArr[status]){
+                    console.log(status.toString()+statusArr[status])
+                }else{
+                    console.log(status.toString()+statusArr['empty'])
+                }
+            },
+            (error) => {
+                console.log('error '+error.toString()+statusArr[error])
+            }
+          )
     }
     
 
@@ -198,18 +270,23 @@ export default class Header extends Component {
                             {/*    isCompleted={this.props.isCompleted}*/}
                             {/*    didCompleted={this.props.didCompleted}*/}
                             {/*/>*/}
-                            <form onSubmit={this.onAsync}>
-                                <input name="name" onChange={this.onNameChange}/>
-                                <input className="btn-gray"  type="submit" placeholder="ADD Item"/>
+                            {/*<form onSubmit={this.onGetStatus}>*/}
+                            {/*    <input name="name" onChange={this.onNameChange}/>*/}
+                            {/*    <input className="btn-gray"  type="submit" placeholder="ADD Item"/>*/}
+                            {/*</form>*/}
+
+                            <form onSubmit={this.onGetData}>
+                                <input name="phone" onChange={this.onPhoneChange}/>
+                                <input name="email" onChange={this.onEmailChange}/>
+                                <input className="btn-gray"  type="submit" placeholder="ADD data"/>
                             </form>
                          
                             {clicked &&
                                 <div className="btn-yellow">Open</div>
                             }
                             <h2 className='header-suptitle'>Distribution Summary</h2>
-                            {ulBlock}
-                            {objectsBlock}
-                            
+                            {/*{ulBlock}*/}
+                            {/*{objectsBlock}*/}
                             
                             <div className='header-main-titleBlock col-md-5 text-center text-md-left col-sm-10'>
                                 {/*<h1 className='header-title'>header title</h1>*/}
